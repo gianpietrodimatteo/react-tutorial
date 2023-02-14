4e20b4e74a229c7604bdc2b790f9a39af821a972
Gianpietro Di Matteo <gianpietro.dimatteo@gmail.com>
2023-02-14 15:50:28 -0300

    Hightliting the squares that caused the win

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import "./index.css";
 
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
+    <button
+      className="square"
+      style={{ background: props.highlite ? "#00CED1" : "#fff" }}
+      onClick={props.onClick}
+    >
       {props.value}
     </button>
   );
@@ -25,9 +29,10 @@ const lines = [
 ];
 
 class Board extends React.Component {
-  renderSquare(i) {
+  renderSquare(i, highlite) {
     return (
       <Square
+        highlite={highlite}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
@@ -37,7 +42,8 @@ class Board extends React.Component {
   renderRow(j) {
     const cols = [];
     for (let k = 0; k < 3; k++) {
-      cols.push(this.renderSquare(k + 3 * j));
+      const index = k + 3 * j;
+      cols.push(this.renderSquare(index, this.props.highlite.includes(index)));
     }
     return <div className="board-row">{cols}</div>;
   }
@@ -118,7 +124,9 @@ class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
+    const highlite = calculateWinner(squares);
+
+    if (highlite || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? "X" : "O";
@@ -148,7 +156,7 @@ class Game extends React.Component {
 
     let status;
     if (winner) {
-      status = "Winner: " + winner;
+      status = "Winner: " + (!this.state.xIsNext ? "X" : "O");
     } else {
       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
@@ -157,6 +165,7 @@ class Game extends React.Component {
       <div className="game">
         <div className="game-board">
           <Board
+            highlite={winner ? winner : [-1, -1, -1]}
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
           />
@@ -182,7 +191,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return [a, b, c];
     }
   }
   return null;
